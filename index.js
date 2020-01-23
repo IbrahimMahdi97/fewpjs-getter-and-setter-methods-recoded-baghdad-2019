064421ac82d0011c38960be875512583ca79d51c
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-23 10:49:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ class Circle {
         this.area = this.pi * (this.radius ** 2)
     }
     set diameter(radius) {
-        this._radius = this.diameter/2
+        this.radius = this.diameter/2
     }
     set area(radius) {
-        this._radius = Math.sqrt (this.area/this.pi)
+        this.radius = Math.sqrt (this.area/this.pi)
     }
     set circumference(radius) {
-        this._radius = this.circumference/(2 * this.pi)
+        this.radius = this.circumference/(2 * this.pi)
     }
 }
